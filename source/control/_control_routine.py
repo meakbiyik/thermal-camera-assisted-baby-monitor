693ad766872982d80f7d7e91a49d7229557cf3f0
Murtaza Eren Akbiyik <erenakbiyik@gmail.com>
2019-02-19 14:38:26 +0300

    Implement transform matrix calculation to the control routine

--- source/control/_control_routine.py
+++ source/control/_control_routine.py
@@ -1,5 +1,7 @@
 import random
 from multiprocessing.queues import Queue
+from skimage import transform
+from transform_matrix import calculate_transform_matrix
 
 def control_routine(rgb_thermal_queue,
                     shared_transform_matrix,
@@ -27,13 +29,35 @@ def control_routine(rgb_thermal_queue,
             # the method timeouts and gives an exception, which is caught below.
             rgb_frame, thermal_frame = rgb_thermal_queue.get(timeout = 60)
                     
-            # Data processing
-            count = 0
-            for i in range(random.randint(1,10) * 10**6):
-                count += 1
-            transform_matrix = (random.randint(1,10),random.randint(1,10))
+            ##########################################################
+            ############# Calculate the transform matrix #############
+            ##########################################################
             
-            # Put data into variables
+            # Increasing the scale factor makes the canny hysteresis thresholds'
+            # calculation unstable. 3 is determined to be an empirically sufficient
+            # number for matriix calculation.
+            scale_factor_of_thermal = 3
+            
+            # override max height and width. 
+            max_height = 60*scale_factor_of_thermal
+            
+            # Expand-reduce frames to have the same size. Do not apply Gaussian smoothing,
+            # since a total-variation denoising will be done later
+            frame_RGB_res = transform.pyramid_reduce(rgb_frame, sigma = 0,
+                                                     downscale = rgb_frame.shape[0]/max_height)
+            frame_thermal_res = transform.pyramid_expand(thermal_frame, sigma = 0,
+                                                         upscale = max_height/thermal_frame.shape[0])
+            
+            # Calculate the transform matrix. Depth 8 is accurate enough for the task.
+            # Increasing does not improve the results, and sometimes makes things
+            # worse since the frames are not homogenously informative enough.
+            transform_matrix = calculate_transform_matrix(frame_RGB_res, frame_thermal_res,
+                                                          division_depth = 8)
+            
+            ##########################################################
+            ######## Put the acquired data into the variables ########
+            ##########################################################
+            
             with room_temp.get_lock():
                 room_temp.value = random.randint(20,30)
             with room_humid.get_lock():
