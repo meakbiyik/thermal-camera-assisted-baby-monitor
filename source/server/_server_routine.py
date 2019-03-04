a0ac963919127cb77c86a0818a36168a4324de22
meakbiyik <erenakbiyik@gmail.com>
2019-03-04 17:46:58 +0300

    Server code for testing

--- source/server/_server_routine.py
+++ source/server/_server_routine.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import numpy as np
 
@@ -15,24 +16,35 @@ def server_routine(frame_queue, audio_queue,
     Maybe multithreading can be added?
     '''
     
-    while True:
+    import cv2
+    writer = cv2.VideoWriter('video.mp4', 0x00000021, float(10.0), (640,480))
+    i = 0
+    
+    try:
+            
+        while True:
+            
+            # Reading the frame 
+            frame = frame_queue.get()
+            
+            # Reading the audio
+            # audio = audio_queue.get()
+            
+            # Reading the values
+            with room_temp.get_lock():
+                current_room_temperature = room_temp.value
+            with room_humid.get_lock():
+                current_room_humidity = room_humid.value
+            with baby_temp.get_lock():
+                current_baby_temperature = baby_temp.value
+            with baby_is_crying.get_lock():
+                crying_detected = baby_is_crying.value
+    
+            writer.write(frame)
+    
+    finally:
         
-        # Reading the frame 
-        frame = frame_queue.get()
+            writer.release()
         
-        # Reading the audio
-        audio = audio_queue.get()
         
-        # Reading the values
-        with room_temp.get_lock():
-            current_room_temperature = room_temp.value
-        with room_humid.get_lock():
-            current_room_humidity = room_humid.value
-        with baby_temp.get_lock():
-            current_baby_temperature = baby_temp.value
-        with baby_is_crying.get_lock():
-            crying_detected = baby_is_crying.value
-        
-        # Print the current inputs to the server to observe the changes
-        sys.stdout.flush()
         
