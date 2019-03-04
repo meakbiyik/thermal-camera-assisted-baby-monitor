f504479937b6438407e044c1bb7fee91edb44442
Hüseyin Ziya İmamoğlu <h.ziya.im@gmail.com>
2019-03-04 17:40:41 +0300

    Update _server_routine.py
    Redundant code deletion

--- source/server/_server_routine.py
+++ source/server/_server_routine.py
@@ -13,6 +13,16 @@ from PIL import Image
 def server_routine(frame_queue, audio_queue,
                    room_temp, room_humid, baby_temp,
                    baby_is_crying):
+    '''
+       Routine that:
+           + Parses the data from all other routines
+           + Synchronizes them if necessary
+           + Sends the data to the server
+
+       This routine needs continuous optimization. Do not overburden it.
+       Maybe multithreading can be added?
+    '''
+
     # Connect a client socket to my_server:8000 (change my_server to the
     # hostname of your server)
     client_socket = socket.socket()
@@ -21,16 +31,8 @@ def server_routine(frame_queue, audio_queue,
     # Make a file-like object out of the connection
     connection = client_socket.makefile('wb')
 
-    '''
-    Routine that:
-        + Parses the data from all other routines
-        + Synchronizes them if necessary
-        + Sends the data to the server
-       
-    This routine needs continuous optimization. Do not overburden it. 
-    Maybe multithreading can be added?
-    '''
 
+    start_stream = False
     want_image = b'w'
     send_image = b's'
 
@@ -38,45 +40,6 @@ def server_routine(frame_queue, audio_queue,
     try:
         stream = io.BytesIO()
         while True:
-
-            if not start_stream:
-                print('Loop entered')
-
-                start = time()
-
-                # Possible addition of non-blocking arguments and select will be considered
-                answer = client_socket.recv(128)
-                print(answer)
-
-                if answer == want_image:
-                    start_stream = True
-                    client_socket.send(send_image)
-
-            else:
-                frame_temp = frame_queue.get()
-
-                if frame_temp is not None:
-                    frame = frame_temp
-
-                print('writing')
-                print(stream.tell())
-                stream.write(frame)
-                print(stream.tell())
-                print(struct.pack('<L', stream.tell()))
-                # Write the length of the capture to the stream and flush to
-                # ensure it actually gets sent
-                connection.write(struct.pack('<L', stream.tell()))
-                connection.flush()
-                # Rewind the stream and send the image data over the wire
-                stream.seek(0)
-                connection.write(stream.read())
-                print(stream.tell())
-                # Reset the stream for the next capture
-                stream.seek(0)
-                stream.truncate()
-
-            # Reading the frame
-
             #         #
             # # Reading the audio
             # audio = audio_queue.get()
@@ -91,20 +54,41 @@ def server_routine(frame_queue, audio_queue,
             # with baby_is_crying.get_lock():
             #     crying_detected = baby_is_crying.value
 
-            # Print the current inputs to the server to observe the changes
-            # print('room temperature: {}'.format(current_room_temperature))
-            # print('room humidity: {}'.format(current_room_humidity))
-            # print('baby temperature: {}'.format(current_baby_temperature))
-            # print('crying detected: {}'.format(crying_detected))
-            # print('frame mean: {}'.format(np.mean(frame)))
-            # print('audio mean: {}'.format(np.mean(audio)))
-            #
-            # sys.stdout.flush()
+            if not start_stream:
+
+
+                start = time()
+
+                # Possible addition of non-blocking arguments and select will be considered
+                answer = client_socket.recv(128)
+
+
+                if answer == want_image:
+                    start_stream = True
+                    client_socket.send(send_image)
+
+            else:
+                frame_temp = frame_queue.get()
+
+                if frame_temp is not None:
+                    frame = frame_temp
+
+                stream.write(frame)
+                # Write the length of the capture to the stream and flush to
+                # ensure it actually gets sent
+                connection.write(struct.pack('<L', stream.tell()))
+                connection.flush()
+                # Rewind the stream and send the image data over the wire
+                stream.seek(0)
+                connection.write(stream.read())
+                # Reset the stream for the next capture
+                stream.seek(0)
+                stream.truncate()
+
+
+
+
+
     finally:
         connection.close()
         client_socket.close()
-        
-
-
-        
-        
